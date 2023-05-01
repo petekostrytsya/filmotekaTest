283c29c0feab3cce165f2fa21c7b205e6e7d4c3f
petekostrytsya <petekostrytsya@gmail.com>
2023-05-02 00:40:06 +0300

    addHeader

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
-// const slider = document.querySelector('.hero-list')
+
 
 const mySwiper = new Swiper('.mySwiper', {
-    // selector: slider,
     slidesPerView: 1,
     loop: true,
     speed: 5000,
@@ -31,3 +30,44 @@ const mySwiper = new Swiper('.mySwiper', {
 //     }
 // }, 3000);
   
+// const body = document.querySelector('.hero-list');
+
+// // Зображення для зміни фонового зображення
+// const images = [
+//   './img/abstract-background-with-red-lines-2x.jpg',
+//   './img/abstract-background-with-yellow-lines-2x.jpg',
+//   './img/abstract-background-with-orange-lines-2x.jpg',
+// ];
+
+// // Задаємо початковий індекс
+// let currentIndex = 0;
+
+// // Встановлюємо інтервал зміни фонового зображення
+// setInterval(() => {
+//   // Встановлюємо фонове зображення body
+//   body.style.backgroundImage = `url(${images[currentIndex]})`;
+
+//   // Інкрементуємо індекс
+//   currentIndex += 1;
+
+//   // Якщо індекс виходить за межі довжини масиву, перевстановлюємо його на 0
+//   if (currentIndex === images.length) {
+//     currentIndex = 0;
+//   }
+// }, 5000);
+
+
+
+let heroList = document.querySelector('.hero-list');
+let heroItems = document.querySelectorAll('.hero-item');
+let counter = 0;
+
+function slideHero() {
+    heroList.style.transform = 'translateX(' + (-100 * counter) + '%)';
+    counter++;
+    if (counter === heroItems.length) {
+        counter = 0;
+    }
+}
+
+let interval = setInterval(slideHero, 5000);
